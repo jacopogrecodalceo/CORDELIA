8f822739a9d73d773849a0841e2c05b878ab2822
jacopogrecodalceo <jacopo.grecodalceo@gmail.com>
2023-10-17 18:08:03 +0200

    real-time update

--- rpr/cordelia_real-time.lua
+++ rpr/cordelia_real-time.lua
@@ -11,10 +11,22 @@ MAIN_TRACK_NAME = '_main'
 -- =================================================================
 -- =================================================================
 
+STATE = true
+PLAY_POS_LAST = 0
+
+NOTEs = {}
+SCOREs = {}
+SCOREs_off = {}
+
+-- =================================================================
+-- =================================================================
+-- =================================================================
+
 local socket = require('socket')
 local s = socket.udp()
 
 function send_to_cordelia(message)
+	--log(message)
 	-- Encode the message as a byte string before sending
 	s:sendto(message, socket.dns.toip('localhost'), 10025)
 end
@@ -40,14 +52,22 @@ function log(e)
 	end
 end
 
--- =================================================================
--- =================================================================
--- =================================================================
+function profile(func, name)
+	local start_time = os.clock() -- Record the start time in seconds
 
-function round(num, decimals)
-	local mult = 10^(decimals or 0)
-	return math.floor(num * mult + 0.5) / mult
+	-- Call the provided function with its arguments
+	local result = func()
+
+	local end_time = os.clock() -- Record the end time in seconds
+
+	local elapsed_time = end_time - start_time -- Calculate elapsed time
+
+	log(string.format("Elapsed time: %.4f seconds -- %s", elapsed_time, name))
 end
+
+-- =================================================================
+-- =================================================================
+-- =================================================================
 
 function extract_csv(string)
 	local elements = {}
@@ -86,241 +106,234 @@ end
 -- =================================================================
 -- =================================================================
 
-function get_all_midi_items(ids, index)
+function get_tracks()
 
-	local instr_name, track, item, take = table.unpack(ids)
+	local tracks = {}
+	local i = 0
 
-	local retval, selected, muted, startppqpos, endppqpos, note_chn, note_pitch, note_velocity = reaper.MIDI_GetNote(take, index)
-	
-	local item_start_pos = reaper.GetMediaItemInfo_Value(item, 'D_POSITION')
-	local item_end_pos = item_start_pos + reaper.GetMediaItemInfo_Value(item, 'D_LENGTH')
-
-	local retval, track_name = reaper.GetSetMediaTrackInfo_String(track, 'P_NAME', 0, 0)
-	
-	local start_pos = reaper.MIDI_GetProjTimeFromPPQPos(take, startppqpos)
-	if start_pos < 0 then
-		start_pos = 0
-	end
-
-	local end_pos = reaper.MIDI_GetProjTimeFromPPQPos(take, endppqpos)
-
-	if start_pos <= item_end_pos and start_pos >= item_start_pos then
-		
-		local dur = end_pos - start_pos
-		local dyn = note_velocity / MIDI_CORRECTION
-		local env = 'classic'
-
-		local freq = reaper.GetTrackMIDINoteNameEx(0, track, note_pitch, note_chn)
-		freq = string.match(freq, 'c%s+(.*)')
-		if not freq then
-			log('You probably forgot to commit your tuning, dumbass')
-			return
-		end
-
-		for _, word in pairs(extract_csv(track_name)) do
-			if word:find("^dur") then
-				local val = word:match("dur%s*(.-)$")
-				dur = load("return " .. dur .. val)()
-			elseif word:find("^dyn") then
-				local val = word:match("dyn%s*(.-)$")
-				dyn = load("return " .. dyn .. val)()
-			elseif word:find("^env%.") then
-				local val = word:match("env%.(%S+)$")
-				env = val
-				--env = if_atk(env)
-			elseif word:find("^freq") then
-				local val = word:match("freq%s*(.-)$")
-				freq = load("return " .. freq .. val)()
-			end
-		end
-
-
-		local retval_text, selected, muted, ppqpos_text, type, note_local_env = reaper.MIDI_GetTextSysexEvt(take, 0)
-
-		local epsilon = 15 -- Adjust the epsilon value based on your desired precision
-		local difference = math.abs(startppqpos - ppqpos_text)
-
-		if note_local_env ~= '' and difference < epsilon then 
-			env = note_local_env
-		end
-
-
-		local params = {
-			start_pos,
-			end_pos,
-			instr_name,
-			dur,
-			dyn,
-			env,
-			freq
-		}
-
-		return params
-	else
-		return nil
-	end
-end
-
-function get_all_text_items(ids)
-
-	local instr_name, track, item, take = table.unpack(ids)
-
-	local start_pos = reaper.GetMediaItemInfo_Value(item, 'D_POSITION')
-	local dur = reaper.GetMediaItemInfo_Value(item, 'D_LENGTH')
-	local end_pos = start_pos+dur
-	local retval, text = reaper.GetSetMediaItemInfo_String(item, 'P_NOTES', 0, 0)
-
-	text = string.gsub(text, "p3", tostring(dur))
-	
-	local params = {
-		start_pos,
-		end_pos,
-		instr_name,
-		dur,
-		text
-	}
-
-	return params
-
-end
-
-local midi_items = {}
-local text_items = {}
-local text_items_off = {}
-
-function get_all_items()
-
-	local index = 0
-	local instr_name = ''
-	local code = ''
-
-	local is_mute = false
-	local is_solo = false
-
-	for i = 0, reaper.CountTracks(0)-1 do
+	while i < reaper.CountTracks(0) do
 
 		local track = reaper.GetTrack(0, i)
-		local track_depth = reaper.GetMediaTrackInfo_Value(track, 'I_FOLDERDEPTH')
+		local is_parent = reaper.GetMediaTrackInfo_Value(track, 'I_FOLDERDEPTH') == 1
+		local is_solo = reaper.GetMediaTrackInfo_Value(track, 'I_SOLO') > 0
+		local is_mute = reaper.GetMediaTrackInfo_Value(track, 'B_MUTE') == 1
 
-		if track_depth == 1 then -- it's a parent
-			local retval, parent_name = reaper.GetSetMediaTrackInfo_String(track, 'P_NAME', 0, 0)
-			instr_name = string.match(parent_name, '@.*')
 
-			is_mute = reaper.GetMediaTrackInfo_Value(track, 'B_MUTE') == 1
-			if reaper.AnyTrackSolo(0) then
-				is_solo = reaper.GetMediaTrackInfo_Value(track, 'I_SOLO') > 0				
-			else
-				is_solo = true
-			end
-		else
-			local parent_track = reaper.GetParentTrack(track)
-			if instr_name ~= '' and parent_track ~= nil then
-				local retval, parent_name = reaper.GetSetMediaTrackInfo_String(parent_track, 'P_NAME', 0, 0)
-				instr_name = string.match(parent_name, '@.*')
-				local is_parent_mute = reaper.GetMediaTrackInfo_Value(parent_track, 'B_MUTE') == 1
-
-				if not is_parent_mute then 
-					is_mute = reaper.GetMediaTrackInfo_Value(track, 'B_MUTE') == 1
-				end
-			end
-		end
-
-		if not is_mute and is_solo then
-			for j = 0, reaper.GetTrackNumMediaItems(track)-1 do
-				local item = reaper.GetTrackMediaItem(track, j)
-				local take = reaper.GetMediaItemTake(item, 0)
-				local ids = {instr_name, track, item, take}
-
-				if take ~= nil and reaper.TakeIsMIDI(take)then -- if ==, it will work on "empty"/text items only
-					local retval, notecnt, ccevtcnt, textsyxevtcnt = reaper.MIDI_CountEvts(take)
-					for index = 0, notecnt-1 do
-						local midi_note = get_all_midi_items(ids, index)
-						if midi_note ~= nil then
-							table.insert(midi_items, midi_note)
-						end
+		
+		if reaper.AnyTrackSolo(0) then
+			if is_solo and is_parent then
+				table.insert(tracks, track)
+				for j = i + 1, reaper.CountTracks(0) - 1 do
+					local sub_track = reaper.GetTrack(0, j)
+					local track_depth = reaper.GetMediaTrackInfo_Value(sub_track, 'I_FOLDERDEPTH')
+					if track_depth == 1 then
+						i = j  -- Set the outer loop index to continue from here
+						break
+					else
+						table.insert(tracks, sub_track)
 					end
-				elseif take == nil then
-					local text_item = get_all_text_items(ids)
-					table.insert(text_item, index)
-					table.insert(text_items, text_item)
-					index = index + 1
-
 				end
 			end
-		end
-	end
-end
-
-function remove_at_play()
-
-	local play_pos = reaper.GetCursorPosition()
-
-	local index = 1
-	while index <= #midi_items do
-
-		local item = midi_items[index]
-		local start_pos = item[1]
-
-		if start_pos < play_pos then
-			table.remove(midi_items, index)
 		else
-			index = index + 1
-		end
-	end
-
-	index = 1
-	while index <= #text_items do
-
-		local item = text_items[index]
-
-		local start_pos = item[1]
-		local end_pos = item[2]
-
-		if start_pos < play_pos and end_pos < play_pos then
-			table.remove(text_items, index)
-		else
-			index = index + 1
-		end
-	end
-
-end
-
-local STATE = true
-local last_play_pos = 0
-
-function on_play()
-	if STATE then
-		last_play_pos = reaper.GetPlayPosition()
-		get_all_items()
-		if #midi_items > 15000 then
-			reaper.CSurf_OnStop()
-			log('Items are more than 150000')
-		end
-		remove_at_play()
-		send_to_cordelia('schedule "heart", 0, -1')
-
-		STATE = false
-	end
-end
-
-function on_stop()
-	if not STATE then
-		send_to_cordelia('turnoff2_i "heart", 0, 0')
-
-		if next(text_items_off) ~= nil then
-			for _, item in pairs(text_items_off) do
-				local instr_num = tostring(item[6] + 300)
-				send_to_cordelia('turnoff2_i ' .. instr_num .. ', 0, 0')
+			if is_mute and is_parent then
+				for j = i + 1, reaper.CountTracks(0) - 1 do
+					local sub_track = reaper.GetTrack(0, j)
+					local track_depth = reaper.GetMediaTrackInfo_Value(sub_track, 'I_FOLDERDEPTH')
+					if track_depth == 1 then
+						table.insert(tracks, track)
+						i = j  -- Set the outer loop index to continue from here
+						break
+					elseif j == reaper.CountTracks(0) - 1 then
+						i = j  -- Set the outer loop index to continue from here
+						break
+					end
+				end
+			elseif not is_mute then
+				table.insert(tracks, track)
 			end
 		end
-		
-		midi_items = {}
-		text_items = {}
-		text_items_off = {}
-		
-		STATE = true
 
+		i = i + 1
+	
 	end
+
+	return tracks
+
+end
+
+function get_item_info(track, j)
+	local id = reaper.GetTrackMediaItem(track, j)
+	local item = {
+		id = id,
+		take = reaper.GetMediaItemTake(id, 0),
+		start_pos = reaper.GetMediaItemInfo_Value(id, 'D_POSITION'),
+		dur = reaper.GetMediaItemInfo_Value(id, 'D_LENGTH')
+	}
+	
+	item.end_pos = item.start_pos + item.dur
+
+	return item
+end
+
+function get_track_name_info(track_name)
+
+	local info = {
+		dur = nil,
+		dyn = nil,
+		env = 'classic',
+		freq = nil
+	}
+
+	for _, word in pairs(extract_csv(track_name)) do
+		if word:find("^dur") then
+			local val = word:match("dur%s*(.-)$")
+			info.dur = val
+		elseif word:find("^dyn") then
+			local val = word:match("dyn%s*(.-)$")
+			info.dyn = val
+		elseif word:find("^env%.") then
+			local val = word:match("env%.(%S+)$")
+			info.env = val
+		elseif word:find("^freq") then
+			local val = word:match("freq%s*(.-)$")
+			info.freq = val
+		end
+	end
+
+	return info
+end
+
+function get_freqs_by_tuning(track)
+	local freqs_by_tuning = {}
+	for i = 0, 127 do
+		local freq = reaper.GetTrackMIDINoteNameEx(0, track, i, 0)
+		freq = string.match(freq, 'c%s+(.*)')
+		if not freq then
+			log('You need to commit the tuning system')
+			return
+		end
+		table.insert(freqs_by_tuning, freq)
+	end
+	return freqs_by_tuning
+end
+
+-- =================================================================
+-- =================================================================
+-- =================================================================
+
+function get_items()
+	
+	local tracks = get_tracks()
+	local item_index = 0
+	local instrument_name 
+	for _, track in pairs(tracks) do
+
+		if reaper.GetMediaTrackInfo_Value(track, 'I_FOLDERDEPTH') == 1 then
+			_, instrument_name = reaper.GetSetMediaTrackInfo_String(track, 'P_NAME', '', false)
+		end 
+
+		local _, track_name = reaper.GetSetMediaTrackInfo_String(track, 'P_NAME', '', false)
+		log(instrument_name)
+		log(track_name)
+
+		for j = 0, reaper.GetTrackNumMediaItems(track)-1 do
+			local item = get_item_info(track, j)
+
+			-- ITEM INFO
+			local item_type = item.take and reaper.TakeIsMIDI(item.take) and 'MIDI' or 'SCORE'
+
+			if item_type == 'MIDI' then
+				local info = get_track_name_info(track_name)
+				info.freqs_by_tuning = get_freqs_by_tuning(track)
+
+				local item_notes = get_notes_from_item(info, item)
+
+				for _, note in pairs(item_notes) do
+					note.instrument_name = instrument_name
+					table.insert(NOTEs, note)
+				end
+			elseif item_type == 'SCORE' then
+				item.index = item_index
+				local score = get_scores_item(item)
+				score.instrument_name = instrument_name
+				table.insert(SCOREs, score)
+				item_index = item_index + 1
+			end
+		end
+	end
+end
+
+function get_notes_from_item(info, item)
+
+	local function apply_dynamic_change(value, info)
+		if info then
+			local expr = load("return " .. value .. info)
+			if type(expr) == "function" then
+				return expr()
+			end
+		end
+		return value
+	end
+
+	local notes = {}
+	local _, notes_count, _, _ = reaper.MIDI_CountEvts(item.take)
+	for index_note = 0, notes_count - 1 do
+		local _, _, is_muted, start_ppqpos, end_ppqpos, chan, pitch, vel, _ = reaper.MIDI_GetNote(item.take, index_note)
+
+		if not is_muted then
+			local onset = reaper.MIDI_GetProjTimeFromPPQPos(item.take, start_ppqpos)
+			onset = onset < 0 and 0 or onset
+
+			if onset <= item.end_pos and onset >= item.start_pos then
+				
+				local end_note = reaper.MIDI_GetProjTimeFromPPQPos(item.take, end_ppqpos)
+
+				local dur = apply_dynamic_change(end_note - onset, info.dur)
+				local dyn = apply_dynamic_change(vel / MIDI_CORRECTION, info.dyn)
+				local env = info.env
+				local freq = apply_dynamic_change(info.freqs_by_tuning[pitch], info.freq)
+
+				table.insert(notes, {
+					onset = onset,
+					end_note = end_note,
+					dur = dur,
+					dyn = dyn,
+					env = env,
+					freq = freq
+				})
+			end
+		end
+	end
+	return notes
+end
+
+function get_scores_item(item)
+	local _, code = reaper.GetSetMediaItemInfo_String(item.id, 'P_NOTES', '', false)
+	code = code:gsub('%bp3%b', tostring(item.dur))
+
+	local score = {
+		onset = item.start_pos,
+		end_pos = item.end_pos,
+		dur = item.dur,
+		code = code,
+		index = item.index
+	}
+
+	return score
+end
+
+-- =================================================================
+-- =================================================================
+-- =================================================================
+
+function cordelia_store()
+
+	local score_file = io.open(file_path, 'w')
+	score_file:write(score_string)  -- Write the text to the file
+	score_file:close() -- Close the file when done
+
+	get_all_items()
+
 end
 
 function get_project_paths()
@@ -341,103 +354,138 @@ function get_project_paths()
 
 end
 
-get_project_paths()
+-- =================================================================
+-- =================================================================
+-- =================================================================
 
-function cordelia_store()
+function safety_play(play_pos)
+	if play_pos - PLAY_POS_LAST > 1 then
+		reaper.CSurf_OnStop()
+	end
+end
 
-	local score_file = io.open(file_path, 'w')
-	score_file:write(score_string)  -- Write the text to the file
-	score_file:close() -- Close the file when done
+function remove_at_play(play_pos, notes, scores)
 
-	get_all_items()
+	for i, note in pairs(NOTEs) do
+		if note.onset < play_pos then
+			table.remove(NOTEs, i)
+		end
+	end
+
+	for i, score in pairs(SCOREs) do
+		if score.onset < play_pos and score.end_pos < play_pos then
+			table.remove(SCOREs, i)
+		end
+	end
 
 end
 
+function sort_by_onset(a, b)
+	return a.onset < b.onset
+end
+
+function on_play(play_pos)
+	if STATE then
+		PLAY_POS_LAST = reaper.GetPlayPosition()
+		get_items()
+		
+		table.sort(NOTEs, sort_by_onset)
+		table.sort(SCOREs, sort_by_onset)
+
+		if #NOTEs > 15000 then
+			reaper.CSurf_OnStop()
+			log('Items are more than 150000')
+		end
+		remove_at_play(play_pos)
+		send_to_cordelia('schedule "heart", 0, -1')
+
+		STATE = false
+	end
+end
+
+function on_stop()
+	if not STATE then
+		send_to_cordelia('turnoff2_i "heart", 0, 0')
+
+		if next(SCOREs_off) ~= nil then
+			for _, score in pairs(SCOREs_off) do
+				send_to_cordelia('turnoff2_i ' .. score.instrument_num .. ', 0, 0')
+			end
+		end
+		
+		NOTEs = {}
+		SCOREs = {}
+		SCOREs_off = {}
+		
+		STATE = true
+
+	end
+end
+
+-- =================================================================
+-- =================================================================
+-- =================================================================
+
 function cordelia_realtime(play_pos)
-
+	
 	if reaper.GetPlayState() == 1 then
-
-		on_play()
 
 		local play_pos = reaper.GetPlayPosition()
 
-		if play_pos - last_play_pos > 1 then
-			reaper.CSurf_OnStop()
-		end
-		
-		last_play_pos = play_pos
+		on_play(play_pos)
+		safety_play(play_pos)
+
+		PLAY_POS_LAST = play_pos
 
 		local index = 1
-		while index <= #midi_items do
-
-			local item = midi_items[index]
-			local start_pos = item[1]
-
-			if start_pos <= play_pos then
+		while index <= #NOTEs do
+			local note = NOTEs[index]
+			if note.onset <= play_pos then
 			
-				local instr_name, dur, dyn, env, freq = item[3], item[4], item[5], item[6], item[7]
-				local csound_string = 'eva_midi ' .. instr_name .. ', 0, ' .. dur .. ', ' .. dyn .. ', ' .. env .. ', ' .. freq
+				local csound_string = 'eva_midi ' .. note.instrument_name .. ', 0, ' .. note.dur .. ', ' .. note.dyn .. ', ' .. note.env .. ', ' .. note.freq
 				send_to_cordelia(csound_string)
-				table.remove(midi_items, index)
+				table.remove(NOTEs, index)
 			else
 				index = index + 1
 			end
 		end
 
 		index = 1
-		while index <= #text_items do
+		while index <= #SCOREs do
+			local score = SCOREs[index]
+			if score.onset <= play_pos then
+				score.instrument_num = tostring(score.index + 300)
 
-			local item = text_items[index]
-
-			local start_pos = item[1]
-			local instr_num = tostring(item[6] + 300)
-
-			if start_pos <= play_pos then
-				
-				local instr_name = item[3]
-				local text = item[5]
-
-				if instr_name == '@cordelia' then
-
-					local csound_string = 'instr ' .. instr_num .. '\n' .. text .. '\nendin\n'
-					csound_string = csound_string .. 'schedule ' .. instr_num .. ', 0, -1'
+				if score.instrument_name == '@cordelia' then
+					local csound_string = 'instr ' .. score.instrument_num .. '\n' .. score.code .. '\nendin\n'
+					csound_string = csound_string .. 'schedule ' .. score.instrument_num .. ', 0, -1'
 					
 					send_to_cordelia(csound_string)
 
-					table.insert(text_items_off, item)
-					table.remove(text_items, index)
+					table.insert(SCOREs_off, score)
+					table.remove(SCOREs, index)
 
 				else
-					local csound_string = insert_after_pattern(text, "%.%w+%(", instr_num .. ', ' .. instr_name .. ', ')
-					--local csound_string = 'instr ' .. instr_num .. '\n' .. table.concat(code, ", ") .. '\nendin\n'
-					--csound_string = csound_string .. 'schedule ' .. instr_num .. ', 0, -1'
+					local csound_string = insert_after_pattern(score.code, "%.%w+%(", score.instrument_num .. ', ' .. score.instrument_name .. ', ')
 					
 					send_to_cordelia(csound_string)
 
-					table.insert(text_items_off, item)
-					table.remove(text_items, index)
+					table.insert(SCOREs_off, score)
+					table.remove(SCOREs, index)
 				end
 			else
 				index = index + 1
 			end
 		end
 
-		if next(text_items_off) ~= nil then
+		if next(SCOREs_off) ~= nil then
 			index = 1
-			while index <= #text_items_off do
-
-				local item = text_items_off[index]
-
-				local end_pos = item[2]
-				local instr_num = tostring(item[6] + 300)
-
-				if end_pos <= play_pos then
-
-					--local csound_string = 'turnoff2_i ' .. instr_num .. ', 0, 0'
-					--send_to_cordelia(csound_string)
-					
-					table.remove(text_items_off, index)
-
+			while index <= #SCOREs_off do
+				local score = SCOREs_off[index]
+				if score.end_pos <= play_pos then
+					local csound_string = 'turnoff2_i ' .. score.instrument_num .. ', 0, 0'
+					send_to_cordelia(csound_string)
+					table.remove(SCOREs_off, index)
 				else
 					index = index + 1
 				end
@@ -446,7 +494,6 @@ function cordelia_realtime(play_pos)
 		
 	elseif reaper.GetPlayState() == 0 then
 		on_stop()
-
 	end
 	
 end
